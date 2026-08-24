da6d411157da0f18d24b64c2cb904e904c46c9bf
dpromstk2000-lab <dpromstk2000@gmail.com>
2026-08-24 22:48:01 +0900

    Add files via upload

--- contact-mail-gateway-r3-config.js
+++ contact-mail-gateway-r3-config.js
@@ -1,22 +1,32 @@
-(() => {
+﻿(() => {
   "use strict";
   window.DPRO_CONTACT_MAIL_GATEWAY_R3_CONFIG = Object.freeze({
-    version: "DPRO-CONTACT-MAIL-GATEWAY-R3-A1-OWNER-DOMAIN-FIRST-20260824-STAGED",
-    status: "STAGED",
-    workerName: "dpro-contact-mail-gateway",
-    workerUrl: "",
-    workerUrlCandidate: "https://dpro-contact-mail-gateway.dpromstk2000.workers.dev",
-    domainPolicy: "OWNER_DOMAIN_FIRST",
-    ownerDomainSubdomainPrefix: "contact",
-    fallbackEnabled: false,
-    fallbackMailDomain: "",
-    fromLocal: "contact",
-    replyLocal: "r",
-    routesKvNamespaceId: "",
-    signingPublicJwk: null,
-    fallbackMailDnsVerified: false,
-    fallbackResendDomainVerified: false,
-    fallbackCloudflareEmailRoutingVerified: false,
-    storesConversationBody: false
-  });
+    "version":  "DPRO-CONTACT-MAIL-GATEWAY-R3-A1-OWNER-DOMAIN-FIRST-20260824-STAGED",
+    "status":  "RUNTIME_READY",
+    "workerName":  "dpro-contact-mail-gateway",
+    "workerUrl":  "https://dpro-contact-mail-gateway.dpromstk2000.workers.dev",
+    "workerUrlCandidate":  "https://dpro-contact-mail-gateway.dpromstk2000.workers.dev",
+    "domainPolicy":  "OWNER_DOMAIN_FIRST",
+    "ownerDomainSubdomainPrefix":  "contact",
+    "fallbackEnabled":  false,
+    "fallbackMailDomain":  "",
+    "fromLocal":  "contact",
+    "replyLocal":  "r",
+    "routesKvNamespaceId":  "31f379ac1fdc4ff782c1094ce67c2ae3",
+    "signingPublicJwk":  {
+                             "key_ops":  [
+                                             "verify"
+                                         ],
+                             "ext":  true,
+                             "kty":  "EC",
+                             "x":  "ll86WoZx9Ss8wL2eJLSRnZZxOtvZzqPAR2M_cZKLeFQ",
+                             "y":  "tJYJYvX6rmax6wZv0oWCON2BTs2GkD0__3Q9wNRhKag",
+                             "crv":  "P-256"
+                         },
+    "fallbackMailDnsVerified":  false,
+    "fallbackResendDomainVerified":  false,
+    "fallbackCloudflareEmailRoutingVerified":  false,
+    "storesConversationBody":  false
+});
 })();
+
